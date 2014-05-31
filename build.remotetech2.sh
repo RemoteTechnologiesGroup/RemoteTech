5388dd7610607b4f71841069c956a9552d4f62e5
Pez Cuckow <email@pezcuckow.com>
2014-05-31 18:11:35 +0100

    Don't set the path
    /doh

--- build.remotetech2.sh
+++ build.remotetech2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PATH=/src/RemoteTech2
+SRCDIR=/src/RemoteTech2
 
-if [ ! -f "$PATH/Assembly-CSharp-firstpass.dll" ] \
-   || [ ! -f "$PATH/Assembly-CSharp.dll" ] \
-   || [ ! -f "$PATH/UnityEngine.dll" ];
+if [ ! -f "$SRCDIR/Assembly-CSharp-firstpass.dll" ] \
+   || [ ! -f "$SRCDIR/Assembly-CSharp.dll" ] \
+   || [ ! -f "$SRCDIR/UnityEngine.dll" ];
 then
    echo "Need to get dependency .dll's"
-   wget "https://www.dropbox.com/s/kyv25p3qn166nzp/dlls.zip?dl=1"
+   wget -O dlls.zip "https://www.dropbox.com/s/kyv25p3qn166nzp/dlls.zip?dl=1"
    unzip dlls.zip -d src/RemoteTech2/
    rm -f dlls.zip
 fi
