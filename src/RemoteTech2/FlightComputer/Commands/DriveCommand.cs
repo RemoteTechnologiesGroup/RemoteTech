09f0959ef810ac84756c4b895be2929ba11d636e
JDP <janus.pedersen@gmail.com>
2014-06-30 03:34:35 +0200

    Added Rover Computer to the Flight Computer
    Largely based on the implementation for the old RT2 repo.
    Rover Computer provides 3 modes:
    -Drive, for driving a set distance or turning a set number of degrees
    -Heading. For keaping a heading for a set distance
    -Coord. For driving autonomously to a set coordinate.
    Coordinates can be typed in manually (pressing enter in any field will
    send the command) or the player can <modifierKey>+left click on the
    ground in either flight or map view to plot in a representation of the
    clicked spot in coordinates. <modifierKey>+left click does the same, it
    just sends the command automatically.

---
+++ src/RemoteTech2/FlightComputer/Commands/DriveCommand.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace RemoteTech
+{
+    public class DriveCommand : AbstractCommand
+    {
+        public enum DriveMode
+        {
+            Off,
+            Turn,
+            Distance,
+            DistanceHeading,
+            Coord
+        }
+
+        public float steering { get; set; }
+        public float target { get; set; }
+        public float target2 { get; set; }
+        public float speed { get; set; }
+        public DriveMode mode { get; set; }
+
+        private bool mAbort;
+
+        public override void Abort() { mAbort = true; }
+
+        public override bool Pop(FlightComputer f)
+        {
+            f.mRoverComputer.InitMode(this);
+            return true;
+        }
+
+        public override bool Execute(FlightComputer f, FlightCtrlState fcs)
+        {
+            if (mAbort)
+            {
+                fcs.wheelThrottle = 0.0f;
+                return true;
+            }
+
+            return f.mRoverComputer.Drive(this, fcs);
+        }
+
+        public static DriveCommand Off()
+        {
+            return new DriveCommand()
+            {
+                mode = DriveMode.Off,
+                TimeStamp = RTUtil.GameTime
+            };
+        }
+
+        public static DriveCommand Turn(float steering, float degrees, float speed)
+        {
+            return new DriveCommand()
+            {
+                mode = DriveMode.Turn,
+                steering = steering,
+                target = degrees,
+                speed = speed,
+                TimeStamp = RTUtil.GameTime
+            };
+        }
+
+        public static DriveCommand Distance(float distance, float steerClamp, float speed)
+        {
+            return new DriveCommand()
+            {
+                mode = DriveMode.Distance,
+                steering = steerClamp,
+                target = distance,
+                speed = speed,
+                TimeStamp = RTUtil.GameTime
+            };
+        }
+
+        public static DriveCommand DistanceHeading(float distance, float heading, float steerClamp, float speed)
+        {
+            return new DriveCommand()
+            {
+                mode = DriveMode.DistanceHeading,
+                steering = steerClamp,
+                target = distance,
+                target2 = heading,
+                speed = speed,
+                TimeStamp = RTUtil.GameTime
+            };
+        }
+
+        public static DriveCommand Coord(float steerClamp, float latitude, float longitude, float speed)
+        {
+            return new DriveCommand()
+            {
+                mode = DriveMode.Coord,
+                steering = steerClamp,
+                target = latitude,
+                target2 = longitude,
+                speed = speed,
+                TimeStamp = RTUtil.GameTime
+            };
+        }
+
+        public override String Description
+        {
+            get
+            {
+                StringBuilder s = new StringBuilder();
+                switch (mode)
+                {
+                    case DriveMode.Coord:
+                        s.Append("Drive to: ");
+                        s.Append(new Vector2(target, target2).ToString("0.000"));
+                        s.Append(" @ ");
+                        s.Append(RTUtil.FormatSI(Math.Abs(speed), "m/s"));
+                        break;
+                    case DriveMode.Distance:
+                        s.Append("Drive: ");
+                        s.Append(RTUtil.FormatSI(target, "m"));
+                        if (speed > 0)
+                            s.Append(" forwards @");
+                        else
+                            s.Append(" backwards @");
+                        s.Append(RTUtil.FormatSI(Math.Abs(speed), "m/s"));
+                        break;
+                    case DriveMode.Turn:
+                        s.Append("Turn: ");
+                        s.Append(target.ToString("0.0"));
+                        if (steering < 0)
+                            s.Append("° right @");
+                        else
+                            s.Append("° left @");
+                        s.Append(Math.Abs(steering).ToString("P"));
+                        s.Append(" steering");
+                        break;
+                    case DriveMode.DistanceHeading:
+                        s.Append("Drive: ");
+                        s.Append(RTUtil.FormatSI(target, "m"));
+                        s.Append(", Hdg: ");
+                        s.Append(target2.ToString("0"));
+                        s.Append("° @ ");
+                        s.Append(RTUtil.FormatSI(Math.Abs(speed), "m/s"));
+                        break;
+                    case DriveMode.Off:
+                        s.Append("Turn rover computer off");
+                        break;
+                }
+
+                return s.ToString() + Environment.NewLine + base.Description;
+            }
+        }
+
+    }
+}
---
+++ src/RemoteTech2/RoverComputer.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace RemoteTech
+{
+
+    class PidController
+    {
+        public /* private */ float mKp, mKd, mKi;
+        private float mOldVal, mOldTime, mOldD;
+        private float McMin, McMax;
+        private float[] mBuffer = null;
+        private int mPtr;
+        private float mSum;
+        private float mValue;
+
+        public PidController(float Kp, float Ki, float Kd,
+                              int integrationBuffer, float clampMin, float clampMax)
+        {
+            mKp = Kp;
+            mKi = Ki;
+            mKd = Kd;
+            McMin = clampMin;
+            McMax = clampMax;
+            if (integrationBuffer >= 1)
+                mBuffer = new float[integrationBuffer];
+            Reset();
+        }
+
+        public void Reset()
+        {
+            mSum = 0;
+            mOldTime = -1;
+            mOldD = 0;
+            if (mBuffer != null)
+                for (int i = 0; i < mBuffer.Length; i++)
+                    mBuffer[i] = 0;
+            mPtr = 0;
+        }
+
+        public void setClamp(float clampMin, float clampMax)
+        {
+            McMin = clampMin;
+            McMax = clampMax;
+        }
+
+        public float Control(float v)
+        {
+            if (Time.fixedTime > mOldTime)
+            {
+                if (mOldTime >= 0)
+                {
+                    mOldD = (v - mOldVal) / (Time.fixedTime - mOldTime);
+
+                    float i = v / (Time.fixedTime - mOldTime);
+                    if (mBuffer != null)
+                    {
+                        mSum -= mBuffer[mPtr];
+                        mBuffer[mPtr] = i;
+                        mPtr++;
+                        if (mPtr >= mBuffer.Length)
+                            mPtr = 0;
+                    }
+                    mSum += i;
+                }
+
+                mOldTime = Time.fixedTime;
+                mOldVal = value;
+            }
+
+            mValue = mKp * v + mKi * mSum + mKd * mOldD;
+
+
+            if (mValue > McMax)
+                mValue = McMax;
+            if (mValue < McMin)
+                mValue = McMin;
+
+
+            return mValue;
+        }
+
+        public float value
+        {
+            get { return mValue; }
+        }
+
+        public static implicit operator float(PidController v)
+        {
+            return v.mValue;
+        }
+    }
+
+    public class RoverComputer
+    {
+        private Vessel mVessel;
+        private PidController mThrottlePID, mWheelPID;
+
+        private float
+            mRoverAlt,
+            mRoverLat,
+            mRoverLon,
+            mTargetLat,
+            mTargetLon,
+            mKeepHDG;
+
+        private Quaternion mRoverRot;
+
+        private Vector3 ForwardAxis;
+
+        public RoverComputer(Vessel v)
+        {
+            mVessel = v;
+            mThrottlePID = new PidController(10f, 1e-5F, 1e-5F, 50, -1f, 1f);
+            mWheelPID = new PidController(10f, 1e-5F, 1e-5F, 50, -1f, 1f);
+        }
+
+        private float RoverHDG
+        {
+            get
+            {
+                Vector3d up = (mVessel.CoM - mVessel.mainBody.position).normalized;
+                Vector3d north = Vector3d.Exclude(up, (mVessel.mainBody.position + mVessel.mainBody.transform.up * (float)mVessel.mainBody.Radius) - mVessel.CoM).normalized;
+                return RTUtil.GetHDG(mVessel.ReferenceTransform.TransformDirection(ForwardAxis), up, north);
+            }
+        }
+
+        private float TargetHDG
+        {
+            get
+            {
+                Vector3d up = (mVessel.CoM - mVessel.mainBody.position).normalized;
+                Vector3d north = Vector3d.Exclude(up, (mVessel.mainBody.position + mVessel.mainBody.transform.up * (float)mVessel.mainBody.Radius) - mVessel.CoM).normalized;
+                return RTUtil.GetHDG((TargetPos - mVessel.CoM).normalized, up, north);
+            }
+        }
+
+        private Vector3 TargetPos
+        {
+            get
+            {
+                return mVessel.mainBody.GetWorldSurfacePosition(mTargetLat, mTargetLon, mVessel.altitude);
+            }
+        }
+
+        private Vector3 RoverOrigPos
+        {
+            get
+            {
+                return mVessel.mainBody.GetWorldSurfacePosition(mRoverLat, mRoverLon, mRoverAlt);
+            }
+        }
+
+        private float RoverSpeed
+        {
+            get
+            {
+                if (ForwardAxis == Vector3.zero)
+                    return (float)mVessel.srf_velocity.magnitude;
+                return Vector3.Dot(mVessel.srf_velocity, mVessel.ReferenceTransform.TransformDirection(ForwardAxis));
+            }
+        }
+
+        public void InitMode(DriveCommand dc)
+        {
+
+            ForwardAxis = Vector3.zero;
+            mRoverAlt = (float)mVessel.altitude;
+            mRoverLat = (float)mVessel.latitude;
+            mRoverLon = (float)mVessel.longitude;
+
+            switch (dc.mode)
+            {
+                case DriveCommand.DriveMode.Turn:
+                    mRoverRot = mVessel.ReferenceTransform.rotation;
+                    break;
+                case DriveCommand.DriveMode.Distance:
+                    mWheelPID.setClamp(-1, 1);
+                    mKeepHDG = RoverHDG;
+                    break;
+                case DriveCommand.DriveMode.DistanceHeading:
+                    mWheelPID.setClamp(-dc.steering, dc.steering);
+                    break;
+                case DriveCommand.DriveMode.Coord:
+                    mWheelPID.setClamp(-dc.steering, dc.steering);
+                    mTargetLat = dc.target;
+                    mTargetLon = dc.target2;
+                    break;
+            }
+            mThrottlePID.Reset();
+            mWheelPID.Reset();
+            mVessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, false);
+        }
+
+        public bool Drive(DriveCommand dc, FlightCtrlState fs)
+        {
+            if (dc != null)
+            {
+                if (ForwardAxis == Vector3.zero && mVessel.srf_velocity.magnitude > 0.1)
+                {
+                    float degForward = Vector3.Angle(mVessel.srf_velocity.normalized, mVessel.ReferenceTransform.forward);
+                    float degUp = Vector3.Angle(mVessel.srf_velocity.normalized, mVessel.ReferenceTransform.up);
+                    float degRight = Vector3.Angle(mVessel.srf_velocity.normalized, mVessel.ReferenceTransform.right);
+
+                    if (degForward < degUp && degForward < degRight)
+                        ForwardAxis = Vector3.forward;
+                    else if (degRight < degUp && degRight < degForward)
+                        ForwardAxis = Vector3.right;
+                    else
+                        ForwardAxis = Vector3.up;
+                }
+
+
+                switch (dc.mode)
+                {
+                    case DriveCommand.DriveMode.Turn:
+                        return Turn(dc, fs);
+                    case DriveCommand.DriveMode.Distance:
+                        return Distance(dc, fs);
+                    case DriveCommand.DriveMode.DistanceHeading:
+                        return DistanceHeading(dc, fs);
+                    case DriveCommand.DriveMode.Coord:
+                        return Coord(dc, fs);
+                }
+                return true;
+            }
+            return true;
+        }
+
+        private bool Turn(DriveCommand dc, FlightCtrlState fs)
+        {
+            if (Math.Abs(Quaternion.Angle(mRoverRot, mVessel.ReferenceTransform.rotation)) < dc.target)
+            {
+                fs.wheelThrottle = mThrottlePID.Control(dc.speed - RoverSpeed);
+                fs.wheelSteer = dc.steering;
+                return false;
+            }
+            else
+            {
+                fs.wheelThrottle = 0;
+                fs.wheelSteer = 0;
+                mVessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, true);
+                dc.mode = DriveCommand.DriveMode.Off;
+                return true;
+            }
+        }
+
+        private bool Distance(DriveCommand dc, FlightCtrlState fs)
+        {
+            if (Vector3.Distance(RoverOrigPos, mVessel.CoM) < Math.Abs(dc.target))
+            {
+                fs.wheelThrottle = mThrottlePID.Control(dc.speed - RoverSpeed);
+                fs.wheelSteer = (dc.speed < 0 ? -1 : 1) * mWheelPID.Control(RTUtil.ClampDegrees180(RoverHDG - mKeepHDG));
+                return false;
+            }
+            else
+            {
+                fs.wheelThrottle = 0;
+                fs.wheelSteer = 0;
+                mVessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, true);
+                dc.mode = DriveCommand.DriveMode.Off;
+                return true;
+            }
+        }
+
+        private bool DistanceHeading(DriveCommand dc, FlightCtrlState fs)
+        {
+            if (Vector3.Distance(RoverOrigPos, mVessel.CoM) < Math.Abs(dc.target))
+            {
+                fs.wheelThrottle = mThrottlePID.Control(dc.speed - RoverSpeed);
+                if (ForwardAxis != Vector3.zero)
+                    fs.wheelSteer = mWheelPID.Control(RTUtil.ClampDegrees180(RoverHDG - dc.target2));
+                return false;
+            }
+            else
+            {
+                fs.wheelThrottle = 0;
+                fs.wheelSteer = 0;
+                mVessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, true);
+                dc.mode = DriveCommand.DriveMode.Off;
+                return true;
+            }
+        }
+
+        private float BrakeSpeed(float speed, float actualSpeed, float distance)
+        {
+            float time = distance / speed;
+            if (time > 5)
+                return speed;
+
+            return Math.Max(speed * time / 5, speed / 10);
+        }
+
+        private bool Coord(DriveCommand dc, FlightCtrlState fs)
+        {
+            float
+                dist = Vector3.Distance(mVessel.CoM, TargetPos),
+                deg = RTUtil.ClampDegrees180(RoverHDG - TargetHDG),
+                speed = RoverSpeed;
+
+            if (dist > Math.Abs(deg) / 36)
+            {
+                fs.wheelThrottle = mThrottlePID.Control(BrakeSpeed(dc.speed, speed, dist) - speed);
+                if (ForwardAxis != Vector3.zero)
+                    fs.wheelSteer = mWheelPID.Control(deg);
+                return false;
+            }
+            else
+            {
+                fs.wheelThrottle = 0;
+                fs.wheelSteer = 0;
+                mVessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, true);
+                dc.mode = DriveCommand.DriveMode.Off;
+                return true;
+            }
+        }
+    }
+}
