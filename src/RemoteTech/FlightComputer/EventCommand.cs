6108793d26bb2f41211a1d44f7529686bb7bb13a
Dennis <peppie23@gmail.com>
2015-01-05 21:50:50 +0100

    EventCommand final integrated

--- src/RemoteTech/FlightComputer/EventCommand.cs
+++ src/RemoteTech/FlightComputer/EventCommand.cs
@@ -7,83 +7,31 @@ namespace RemoteTech
 {
     public class EventCommand : AbstractCommand
     {
-        public class FakeEventFromLoad
-        {
-            public string Name;
-            public string GUIName;
-            public string PartInfo;
-            public int PartId;
+        [Persistent] public string GUIName;
+        [Persistent] public int PartId;
+        [Persistent] public string Module;
 
-            public void Invoke()
-            {
-                BaseEvent b = FlightGlobals.ActiveVessel.parts.ElementAt(PartId).Events.Where(e => e.GUIName == GUIName).FirstOrDefault();
-                if (b != null)
-                    b.Invoke();
-            }
-        };
-
-        public BaseEvent BaseEvent;
-        public FakeEventFromLoad loadedEvent;
+        public BaseEvent BaseEvent = null;
 
         public override String Description
         {
             get
             {
-                string desc = "";
-
-                if (loadedEvent != null)
-                {
-                    desc = loadedEvent.PartInfo + ": " + loadedEvent.GUIName;
-                }
-                else
-                {
-                    desc = BaseEvent.listParent.part.partInfo.title + ": " + BaseEvent.GUIName;
-                }
-
-                desc += Environment.NewLine + base.Description;
-
-                return desc;
-
+                return BaseEvent.listParent.part.partInfo.title + ": " + BaseEvent.GUIName +
+                        Environment.NewLine + base.Description;
             }
         }
         public override string ShortName
         {
             get
             {
-                if (loadedEvent != null)
-                {
-                    return loadedEvent.GUIName;
-                }
                 return BaseEvent.GUIName;
             }
         }
          
         public override bool Pop(FlightComputer f)
         {
-            RTLog.Notify("############ BaseEvent Debug ##########");
-            var partid = FlightGlobals.ActiveVessel.parts.ToList().IndexOf(BaseEvent.listParent.part);
-            RTLog.Notify("PartId: {0}",partid);
-            Part part = FlightGlobals.ActiveVessel.parts[partid];
-            RTLog.Notify("partname: {0}", part.name);
-            RTLog.Notify("----- Actions ----");
-            foreach(var a in part.Actions){
-                RTLog.Notify("- Action: ",a.name);
-            }
-            RTLog.Notify("----- Events ----");
-            foreach (var e in part.Events)
-            {
-                RTLog.Notify("- Event: ", e.name);
-                RTLog.Notify("- Event: ", e.GUIName);
-            }
-
-            if (loadedEvent != null)
-            {
-                loadedEvent.Invoke();
-            }
-            else
-            {
-                //BaseEvent.Invoke();
-            }
+            BaseEvent.Invoke();
             
             return false;
         }
@@ -93,34 +41,31 @@ namespace RemoteTech
             return new EventCommand()
             {
                 BaseEvent = ev,
+                GUIName = ev.GUIName,
+                PartId = FlightGlobals.ActiveVessel.parts.ToList().IndexOf(ev.listParent.part),
+                Module = ev.listParent.module.ClassName.ToString(),
                 TimeStamp = RTUtil.GameTime,
             };
         }
 
+        /// <summary>
+        /// Load infos into this object and create a new BaseEvent
+        /// </summary>
         public override void Load(ConfigNode n, FlightComputer fc)
         {
             base.Load(n, fc);
-            var bn = n.GetNode("BaseEvent");
 
-            var fe = new FakeEventFromLoad();
-            fe.Name = bn.GetValue("Name");
-            fe.PartInfo = bn.GetValue("PartInfo");
-            fe.PartId = int.Parse(bn.GetValue("PartId"));
-            fe.GUIName = bn.GetValue("GuiName");
+            PartId = int.Parse(n.GetValue("PartId"));
+            Module = n.GetValue("Module");
+            GUIName = n.GetValue("GUIName");
 
-            loadedEvent = fe;
-        }
-
-        public override void Save(ConfigNode n, FlightComputer fc)
-        {
-            base.Save(n, fc);
-            var bn = this.getCommandConfigNode(n).AddNode("BaseEvent");
-
-            bn.AddValue("Name", BaseEvent.name);
-            bn.AddValue("GuiName", BaseEvent.GUIName);
-            bn.AddValue("PartInfo", BaseEvent.listParent.part.partInfo.title);
-            int partid = FlightGlobals.ActiveVessel.parts.ToList().IndexOf(BaseEvent.listParent.part);
-            bn.AddValue("PartId", partid);
+            Part part = FlightGlobals.ActiveVessel.parts[PartId];
+            PartModule partmodule = part.Modules[Module];
+            BaseEventList eventlist = new BaseEventList(part, partmodule);
+            if (eventlist.Count > 0)
+            {
+                BaseEvent = eventlist.Where(ba => ba.GUIName == GUIName).FirstOrDefault();
+            }
         }
     }
 }
