78ce2a501f642dae346de98fe3daeac4cc47c933
Chris Woerz <erendrake@gmail.com>
2015-01-06 11:14:57 -0700

    require at least one crew for command

--- src/RemoteTech/Modules/ProtoSignalProcessor.cs
+++ src/RemoteTech/Modules/ProtoSignalProcessor.cs
@@ -5,37 +5,61 @@ namespace RemoteTech
 {
     public class ProtoSignalProcessor : ISignalProcessor
     {
+        private readonly ProtoPartModuleSnapshot protoPart;
+        private readonly Vessel vessel;
+
         public String Name { get { return String.Format("ProtoSignalProcessor({0})", VesselName); } }
-        public bool Visible { get { return MapViewFiltering.CheckAgainstFilter(mVessel); } }
-        public CelestialBody Body { get { return mVessel.mainBody; } }
-        public Vector3 Position { get { return mVessel.GetWorldPos3D(); } }
-        public String VesselName { get { return mVessel.vesselName; } set { mVessel.vesselName = value; } }
+
+        public bool Visible { get { return MapViewFiltering.CheckAgainstFilter(vessel); } }
+
+        public CelestialBody Body { get { return vessel.mainBody; } }
+
+        public Vector3 Position { get { return vessel.GetWorldPos3D(); } }
+
+        public String VesselName { get { return vessel.vesselName; } set { vessel.vesselName = value; } }
+
         public bool VesselLoaded { get { return false; } }
+
         public bool Powered { get; private set; }
+
         public bool IsCommandStation { get; private set; }
-        public Guid Guid { get { return mVessel.id; } }
-        public Vessel Vessel { get { return mVessel; } }
+
+        public Guid Guid { get { return vessel.id; } }
+
+        public Vessel Vessel { get { return vessel; } }
+
         public FlightComputer FlightComputer { get { return null; } }
+
         public bool IsMaster { get { return true; } }
-        private readonly Vessel mVessel;
+
 
         public ProtoSignalProcessor(ProtoPartModuleSnapshot ppms, Vessel v)
         {
-            mVessel = v;
-            Powered = ppms.GetBool("IsRTPowered");
+            protoPart = ppms;
+            vessel = v;
+            Initialize();
+        }
 
-	    int commandMinCrew = RTSettings.Instance.DefaultMinimumCrew;
-			
-            try {
-                commandMinCrew = ppms.GetInt("RTCommandMinCrew");
-                RTLog.Notify("ProtoSignalProcessor(Powered: {0}, HasCommandStation: {1}, Crew: {2}/{3})", 
-                    Powered, v.HasCommandStation(), v.GetVesselCrew().Count, commandMinCrew));
-            } catch (ArgumentException) {
-                // I'm assuming this would get thrown by ppms.GetInt()... do the other functions have an exception spec?
-                RTLog.Notify("ProtoSignalProcessor(Powered: {0}, HasCommandStation: {1}, Crew: {2}/{3} [Default]", 
-                    Powered, v.HasCommandStation(), v.GetVesselCrew().Count, RTSettings.Instance.DefaultMinimumCrew);
+        private void Initialize()
+        {
+            Powered = protoPart.GetBool("IsRTPowered");
+
+            int commandMinCrew = RTSettings.Instance.DefaultMinimumCrew;
+
+            try
+            {
+                commandMinCrew = protoPart.GetInt("RTCommandMinCrew");
+                RTLog.Notify("ProtoSignalProcessor(Powered: {0}, HasCommandStation: {1}, Crew: {2}/{3})",
+                    Powered, vessel.HasCommandStation(), vessel.GetVesselCrew().Count, commandMinCrew);
             }
-            IsCommandStation = Powered && v.HasCommandStation() && v.GetVesselCrew().Count >= commandMinCrew;
+            catch (ArgumentException)
+            {
+                // I'm assuming this would get thrown by ppms.GetInt()... do the other functions have an exception spec?
+                RTLog.Notify("ProtoSignalProcessor(Powered: {0}, HasCommandStation: {1}, Crew: {2}/{3} [Default]",
+                    Powered, vessel.HasCommandStation(), vessel.GetVesselCrew().Count, RTSettings.Instance.DefaultMinimumCrew);
+            }
+            var hasEnoughCrew = vessel.GetVesselCrew().Count >= Math.Max(commandMinCrew, 1);
+            IsCommandStation = Powered && vessel.HasCommandStation() && hasEnoughCrew;
         }
 
         public override String ToString()
