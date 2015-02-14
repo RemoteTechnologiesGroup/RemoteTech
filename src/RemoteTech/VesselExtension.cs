f5ae124354c38f2641786d4c16f4567a07b1a489
Chris Woerz <erendrake@gmail.com>
2015-02-13 22:21:53 -0700

    trying again on getting the new api call right

---
+++ src/RemoteTech/VesselExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace RemoteTech
+{
+    public static class VesselExtension
+    {
+        public static bool HasLocalControl(this Vessel vessel)
+        {
+            return vessel.parts.Any(p => p.isControlSource && (p.protoModuleCrew.Any() || !p.FindModulesImplementing<ISignalProcessor>().Any()));
+        }
+    }
+}
