7450c6ea42cfae257d235a806382ed1a2a5bf994
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2015-09-26 19:49:24 +0200

    Add NoTargetGuid to backupFields()

--- src/RemoteTech/RTSettings.cs
+++ src/RemoteTech/RTSettings.cs
@@ -101,7 +101,7 @@ namespace RemoteTech
         }
 
         /// <summary>
-        /// Stores the MapFilter, ActiveVesselGuid and RemoteTechEnabled Value for overriding
+        /// Stores the MapFilter, ActiveVesselGuid, NoTargetGuid and RemoteTechEnabled Value for overriding
         /// with third party settings
         /// </summary>
         public void backupFields()
@@ -109,6 +109,7 @@ namespace RemoteTech
             backupNode = new ConfigNode();
             backupNode.AddValue("MapFilter", MapFilter);
             backupNode.AddValue("ActiveVesselGuid", ActiveVesselGuid);
+            backupNode.AddValue("NoTargetGuid", NoTargetGuid);
             backupNode.AddValue("RemoteTechEnabled", RemoteTechEnabled);
         }
 
