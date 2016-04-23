827916e1cb3601b2d597e2d0162fb736a6ce9abb
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2016-04-23 21:44:52 +0200

    Update Travis dll files to KSP 1.1

--- build.remotetech.sh
+++ build.remotetech.sh
@@ -16,7 +16,7 @@ then
 
    if [[ ! -f dlls.zip ]]; then
       echo "Need to get dependency .dll's"
-      wget -O dlls.zip "https://docs.google.com/a/atteo.com/uc?authuser=0&id=0B7ey9r52itVxUVB3eWxpTDhpWlk&export=download"
+      wget -O dlls.zip "https://www.dropbox.com/s/nyugqmxniuk30f8/kspdll-1.1.zip?dl=0"
    fi
    
    if [ -z "$ZIPPASSWORD" ]; then
