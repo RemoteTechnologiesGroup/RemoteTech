88f1fe9b58b5999fc9ed26b294b08bcdf63ad9a7
neitsa <neitsa@gmail.com>
2016-10-07 19:06:26 +0200

    Rework code on the flight UI patcher (don't remove all listeners anymore).

--- src/RemoteTech/UI/FlightUIPatcher.cs
+++ src/RemoteTech/UI/FlightUIPatcher.cs
@@ -1,4 +1,6 @@
-﻿using KSP.UI.Screens.Flight;
+﻿using System.Collections.Generic;
+using System.Linq;
+using KSP.UI.Screens.Flight;
 using RemoteTech.FlightComputer.Commands;
 
 namespace RemoteTech.UI
@@ -6,31 +8,42 @@ namespace RemoteTech.UI
     public class FlightUIPatcher
     {
         /// <summary>
+        /// Action groups corresponding to the GUI buttons we want to hook / patch.
+        /// </summary>
+        public static KSPActionGroup[] PatchedActionGroups = { KSPActionGroup.Gear, KSPActionGroup.Brakes, KSPActionGroup.Light, KSPActionGroup.Abort };
+
+        /// <summary>
         /// Hook flight action group buttons: gear, brakes, light and abort buttons.
         /// </summary>
         public static void Patch()
-        {
-            ActionGroupToggleButton[] actionGroupToggleButtons = UnityEngine.Object.FindObjectsOfType<ActionGroupToggleButton>();
-            for (int i = 0; i < actionGroupToggleButtons.Length; ++i)
+        {   
+            var buttons = CollectActionGroupToggleButtons(PatchedActionGroups);
+            for (int i = 0; i < buttons.Count; ++i)
             {
-                ActionGroupToggleButton button = actionGroupToggleButtons[i];
-                switch(button.group)
-                {
-                    //only for the 4 groups below: not SAS or RCS action groups.
-                    case KSPActionGroup.Gear:
-                    case KSPActionGroup.Brakes:
-                    case KSPActionGroup.Light:
-                    case KSPActionGroup.Abort:
-                        {
-                            // remove all current listeners and set our hook
-                            button.toggle.onToggle.RemoveAllListeners();
-                            button.toggle.onToggle.AddListener( () => ActivateActionGroup(button.group) );
-                            break;
-                        }
-                }
+                // set our hook
+                buttons[i].toggle.onToggle.AddListener( () => ActivateActionGroup(buttons[i].group) );
             }
         }
 
+        /// <summary>
+        /// Get action groups buttons depending on their group.
+        /// </summary>
+        /// <param name="actionGroups">The action group(s) in which the buttons should be.</param>
+        /// <returns>A list of action ActionGroupToggleButton buttons, filter by actionGroups paramter.</returns>
+        private static List<ActionGroupToggleButton> CollectActionGroupToggleButtons(KSPActionGroup[] actionGroups)
+        {
+            // get all action group buttons
+            ActionGroupToggleButton[] actionGroupToggleButtons = UnityEngine.Object.FindObjectsOfType<ActionGroupToggleButton>();
+            // filter them to only get the buttons that have a group in the actionGroups array
+            var buttons = actionGroupToggleButtons.Where(button => actionGroups.Any(ag => button.group == ag)).ToList();
+
+            return buttons;
+        }
+
+        /// <summary>
+        /// Called when an action group button, from the KSP GUI, is pressed.
+        /// </summary>
+        /// <param name="ag">The action group that was pressed.</param>
         private static void ActivateActionGroup(KSPActionGroup ag)
         {
             var satellite = RTCore.Instance.Satellites[FlightGlobals.ActiveVessel];
