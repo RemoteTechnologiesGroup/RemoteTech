875e546d5832250bf44aaf753658ab45b11964c9
neitsa <neitsa@gmail.com>
2016-11-07 17:41:49 +0100

    Typo in VesselExtension.

--- src/RemoteTech/VesselExtension.cs
+++ src/RemoteTech/VesselExtension.cs
@@ -5,14 +5,14 @@ namespace RemoteTech
     public static class VesselExtension
     {
         /// <summary>
-        /// Get wether a vessel has local control or not (that is, if it's kerbal controlled or not).
+        /// Get whether a vessel has local control or not (that is, if it's Kerbal controlled or not).
         /// </summary>
         /// <param name="vessel">The vessel to check for.</param>
         /// <returns>true if the vessel has a local control, false otherwise.</returns>
         public static bool HasLocalControl(this Vessel vessel)
         {
             // vessel must be a control source and it must be crewed or not implementing a module processor
-            bool hasLocalControl = vessel.parts.Any(p => (p.isControlSource > Vessel.ControlLevel.NONE) && (p.protoModuleCrew.Any() || !p.FindModulesImplementing<ISignalProcessor>().Any()));
+            var hasLocalControl = vessel.parts.Any(p => (p.isControlSource > Vessel.ControlLevel.NONE) && (p.protoModuleCrew.Any() || !p.FindModulesImplementing<ISignalProcessor>().Any()));
             if (!hasLocalControl)
             {
                 // check if theres's a SPU which is a command station. 
