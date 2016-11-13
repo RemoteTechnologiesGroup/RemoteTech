adfb4ee196ccfaa440df1cb3b10095665b7a7863
neitsa <neitsa@gmail.com>
2016-11-13 18:19:17 +0100

    Move Quaternion extension method to its own file.
    - note: code is from MechJeb.

---
+++ src/RemoteTech-Common/Extensions/QuaternionExtension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RemoteTech.Common.Extensions
+{
+    public static class QuaternionExtension
+    {
+        //  Functions copied from MechJeb master on 18.04.2016
+        //TODO: remove this on FlightComputer overhauling if we don't need those functions anymore.
+
+        public static Vector3d DeltaEuler(this Quaternion delta)
+        {
+            return new Vector3d(
+                (delta.eulerAngles.x > 180) ? (delta.eulerAngles.x - 360.0F) : delta.eulerAngles.x,
+                -((delta.eulerAngles.y > 180) ? (delta.eulerAngles.y - 360.0F) : delta.eulerAngles.y),
+                (delta.eulerAngles.z > 180) ? (delta.eulerAngles.z - 360.0F) : delta.eulerAngles.z
+                );
+        }
+    }
+}
--- src/RemoteTech-Common/Utils/RTUtil.cs
+++ src/RemoteTech-Common/Utils/RTUtil.cs
@@ -10,23 +10,5 @@ using UnityEngine;
 
 namespace RemoteTech.Common.Utils
 {
-    public static partial class RTUtil
-    {
 
-        // -----------------------------------------------
-        // Copied from MechJeb master on 18.04.2016
-        public static Vector3d DeltaEuler(this Quaternion delta) 
-        {
-            return new Vector3d(
-                (delta.eulerAngles.x > 180) ? (delta.eulerAngles.x - 360.0F) : delta.eulerAngles.x,
-                -((delta.eulerAngles.y > 180) ? (delta.eulerAngles.y - 360.0F) : delta.eulerAngles.y),
-                (delta.eulerAngles.z > 180) ? (delta.eulerAngles.z - 360.0F) : delta.eulerAngles.z
-                );
-        }
-
-
-
-        // end MechJeb import
-        //---------------------------------------
-    }
 }
