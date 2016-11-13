5ea397b2a7e9065f53409377469ff0cfa705dd72
neitsa <neitsa@gmail.com>
2016-11-13 18:00:46 +0100

    Move PartModule extension methods to their own file.

---
+++ src/RemoteTech-Common/Extensions/PartModuleExtension.cs
@@ -0,0 +1,23 @@
+﻿
+namespace RemoteTech.Common.Extensions
+{
+    public static class PartModuleExtension
+    {
+        public static bool IsAntenna(this PartModule pm)
+        {
+            return pm.Fields.GetValue<bool>("IsRTAntenna") &&
+                   pm.Fields.GetValue<bool>("IsRTPowered") &&
+                   pm.Fields.GetValue<bool>("IsRTActive");
+        }
+
+        public static bool IsCommandStation(this PartModule pm)
+        {
+            return pm.Fields.GetValue<bool>("IsRTCommandStation");
+        }
+
+        public static bool IsSignalProcessor(this PartModule pm)
+        {
+            return pm.Fields.GetValue<bool>("IsRTSignalProcessor");
+        }
+    }
+}
--- src/RemoteTech-Common/Utils/RTUtil.cs
+++ src/RemoteTech-Common/Utils/RTUtil.cs
@@ -208,32 +208,4 @@ namespace RemoteTech.Common.Utils
         // end MechJeb import
         //---------------------------------------
     }
-
-
-    public static partial class RTUtil
-    {
-
-
-        public static bool IsAntenna(this PartModule pm)
-        {
-            return pm.Fields.GetValue<bool>("IsRTAntenna") &&
-                   pm.Fields.GetValue<bool>("IsRTPowered") &&
-                   pm.Fields.GetValue<bool>("IsRTActive");
-        }
-
-        public static bool IsCommandStation(this PartModule pm)
-        {
-            return pm.Fields.GetValue<bool>("IsRTCommandStation");
-        }
-
-        public static bool IsSignalProcessor(this PartModule pm)
-        {
-            return pm.Fields.GetValue<bool>("IsRTSignalProcessor");
-        }
-
-
-
-
-
-    }
 }
