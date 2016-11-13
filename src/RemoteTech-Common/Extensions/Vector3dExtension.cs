c6529f520ddb1a212c6a66c5a83d7fa5931fa0d7
neitsa <neitsa@gmail.com>
2016-11-13 18:15:15 +0100

    Move Vector3d extension methods to their own file.
    - note: code is from MechJeb.

---
+++ src/RemoteTech-Common/Extensions/Vector3dExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RemoteTech.Common.Extensions
+{
+    public static class Vector3DExtension
+    {
+        //  All functions copied from MechJeb master on 18.04.2016
+        //TODO: remove this on FlightComputer overhauling if we don't need those functions anymore.
+
+        public static Vector3d Invert(this Vector3d vector)
+        {
+            return new Vector3d(1 / vector.x, 1 / vector.y, 1 / vector.z);
+        }
+
+        public static Vector3d Reorder(this Vector3d vector, int order)
+        {
+            switch (order)
+            {
+                case 123:
+                    return new Vector3d(vector.x, vector.y, vector.z);
+                case 132:
+                    return new Vector3d(vector.x, vector.z, vector.y);
+                case 213:
+                    return new Vector3d(vector.y, vector.x, vector.z);
+                case 231:
+                    return new Vector3d(vector.y, vector.z, vector.x);
+                case 312:
+                    return new Vector3d(vector.z, vector.x, vector.y);
+                case 321:
+                    return new Vector3d(vector.z, vector.y, vector.x);
+            }
+            throw new ArgumentException("Invalid order", nameof(order));
+        }
+
+        public static Vector3d Sign(this Vector3d vector)
+        {
+            return new Vector3d(Math.Sign(vector.x), Math.Sign(vector.y), Math.Sign(vector.z));
+        }
+
+        public static Vector3d Clamp(this Vector3d value, double min, double max)
+        {
+            return new Vector3d(
+                Clamp(value.x, min, max),
+                Clamp(value.y, min, max),
+                Clamp(value.z, min, max)
+                );
+        }
+    }
+}
--- src/RemoteTech-Common/Utils/RTUtil.cs
+++ src/RemoteTech-Common/Utils/RTUtil.cs
@@ -12,7 +12,7 @@ namespace RemoteTech.Common.Utils
 {
     public static partial class RTUtil
     {
- 
+
         // -----------------------------------------------
         // Copied from MechJeb master on 18.04.2016
         public static Vector3d DeltaEuler(this Quaternion delta) 
@@ -24,43 +24,7 @@ namespace RemoteTech.Common.Utils
                 );
         }
 
-        public static Vector3d Invert(this Vector3d vector) 
-        {
-            return new Vector3d(1 / vector.x, 1 / vector.y, 1 / vector.z);
-        }
 
-        public static Vector3d Reorder(this Vector3d vector, int order) 
-        {
-            switch (order) {
-                case 123:
-                    return new Vector3d(vector.x, vector.y, vector.z);
-                case 132:
-                    return new Vector3d(vector.x, vector.z, vector.y);
-                case 213:
-                    return new Vector3d(vector.y, vector.x, vector.z);
-                case 231:
-                    return new Vector3d(vector.y, vector.z, vector.x);
-                case 312:
-                    return new Vector3d(vector.z, vector.x, vector.y);
-                case 321:
-                    return new Vector3d(vector.z, vector.y, vector.x);
-            }
-            throw new ArgumentException("Invalid order", nameof(order));
-        }
-
-        public static Vector3d Sign(this Vector3d vector) 
-        {
-            return new Vector3d(Math.Sign(vector.x), Math.Sign(vector.y), Math.Sign(vector.z));
-        }
-
-        public static Vector3d Clamp(this Vector3d value, double min, double max) 
-        {
-            return new Vector3d(
-                Clamp(value.x, min, max),
-                Clamp(value.y, min, max),
-                Clamp(value.z, min, max)
-                );
-        }
 
         // end MechJeb import
         //---------------------------------------
