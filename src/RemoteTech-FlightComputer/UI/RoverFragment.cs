b169f7f3b9ef4aadbf15790c7285632135309ac8
neitsa <neitsa@gmail.com>
2016-11-16 23:50:39 +0100

    RoverFragment code cleanup (no logic change).

--- src/RemoteTech-FlightComputer/UI/RoverFragment.cs
+++ src/RemoteTech-FlightComputer/UI/RoverFragment.cs
@@ -1,37 +1,40 @@
 ﻿using System;
+using System.Globalization;
 using RemoteTech.Common.Extensions;
 using RemoteTech.Common.UI;
 using RemoteTech.Common.Utils;
+using RemoteTech.FlightComputer.Commands;
 using UnityEngine;
 
-namespace RemoteTech.UI
+namespace RemoteTech.FlightComputer.UI
 {
     public class RoverFragment : IFragment
     {
-        private FlightComputer.FlightComputer mFlightComputer;
-        private Action mOnClickQueue;
+        private readonly FlightComputer _flightComputer;
+        private readonly Action _onClickQueue;
 
-        private bool distDefault = true;
+        private bool _distDefault = true;
 
-        private float
-            mSteering = 0,
-            mSteerClamp = 1,
-            prevTurn = 0,
-            prevDist = 0;
+        private float _steering;
+        private float _steerClamp = 1;
+        private float _prevTurn;
+        private float _prevDist;
 
-        private string
-            mTurn = "",
-            mSpeed = "5",
-            mDist = "",
-            mLatitude = "",
-            mLongditude = "",
-            Mheading = "";
+        private string _turnString = "";
+        private string _speedString = "5";
+        private string _distanceString = "";
+        private string _latitudeString = "";
+        private string _longitudeString = "";
+        private string _headingString = "";
 
+        int _selected;
+        private bool _mouseClick;
+        private readonly string[] _tabs = { "Tgt.", "Hdt.", "Fine" };
 
-        public RoverFragment(FlightComputer.FlightComputer fc, Action queue)
+        public RoverFragment(FlightComputer fc, Action queue)
         {
-            mFlightComputer = fc;
-            mOnClickQueue = queue;
+            _flightComputer = fc;
+            _onClickQueue = queue;
         }
 
         private float Turn
@@ -39,12 +42,12 @@ namespace RemoteTech.UI
             get
             {
                 float turn;
-                if (!Single.TryParse(mTurn, out turn)) {
+                if (!float.TryParse(_turnString, out turn)) {
                     turn = 0;
                 }
                 return turn;
             }
-            set { mTurn = value.ToString(); }
+            set { _turnString = value.ToString(CultureInfo.InvariantCulture); }
         }
 
         private float Heading
@@ -52,12 +55,12 @@ namespace RemoteTech.UI
             get
             {
                 float heading;
-                if (!Single.TryParse(Mheading, out heading)) {
+                if (!float.TryParse(_headingString, out heading)) {
                     heading = 0;
                 }
                 return heading;
             }
-            set { mTurn = value.ToString(); }
+            set { _turnString = value.ToString(CultureInfo.InvariantCulture); }
         }
 
         private float Speed
@@ -65,12 +68,12 @@ namespace RemoteTech.UI
             get
             {
                 float speed;
-                if (!Single.TryParse(mSpeed, out speed)) {
+                if (!float.TryParse(_speedString, out speed)) {
                     speed = 0;
                 }
                 return speed;
             }
-            set { mSpeed = value.ToString(); }
+            set { _speedString = value.ToString(CultureInfo.InvariantCulture); }
         }
 
         private float Dist
@@ -78,12 +81,12 @@ namespace RemoteTech.UI
             get
             {
                 float dist;
-                if (!Single.TryParse(mDist, out dist)) {
+                if (!float.TryParse(_distanceString, out dist)) {
                     dist = 0;
                 }
                 return dist;
             }
-            set { mDist = value.ToString(); }
+            set { _distanceString = value.ToString(CultureInfo.InvariantCulture); }
         }
 
         private float Latitude
@@ -91,12 +94,12 @@ namespace RemoteTech.UI
             get
             {
                 float latitude;
-                if (!Single.TryParse(mLatitude, out latitude)) {
+                if (!float.TryParse(_latitudeString, out latitude)) {
                     latitude = 0;
                 }
                 return latitude;
             }
-            set { mLatitude = value.ToString(); }
+            set { _latitudeString = value.ToString(CultureInfo.InvariantCulture); }
         }
 
         private float Longitude
@@ -104,25 +107,22 @@ namespace RemoteTech.UI
             get
             {
                 float longitude;
-                if (!Single.TryParse(mLongditude, out longitude)) {
+                if (!float.TryParse(_longitudeString, out longitude)) {
                     longitude = 0;
                 }
                 return longitude;
             }
-            set { mLongditude = value.ToString(); }
+            set { _longitudeString = value.ToString(CultureInfo.InvariantCulture); }
         }
 
         private void EnqueueTurn()
         {
-            mFlightComputer.Enqueue(DriveCommand.Turn(mSteering, Turn, Speed));
+            _flightComputer.Enqueue(DriveCommand.Turn(_steering, Turn, Speed));
         }
         private void EnqueueDist()
         {
-            mFlightComputer.Enqueue(DriveCommand.Distance(Dist, 0, Speed));
+            _flightComputer.Enqueue(DriveCommand.Distance(Dist, 0, Speed));
         }
-        int selected = 0;
-        private bool MouseClick = false;
-        private readonly string[] Tabs = { "Tgt.", "Hdt.", "Fine" };
 
         public void Draw()
         {
@@ -130,9 +130,9 @@ namespace RemoteTech.UI
 
             GUILayout.BeginVertical(GUILayout.Width(156), GUILayout.Height(300));
             {
-                selected = GUILayout.Toolbar(selected, Tabs, GUILayout.Width(160 - GUI.skin.button.margin.right * 2.0f));
+                _selected = GUILayout.Toolbar(_selected, _tabs, GUILayout.Width(160 - GUI.skin.button.margin.right * 2.0f));
 
-                switch (selected) {
+                switch (_selected) {
                     case 0:
                         Target();
                         break;
@@ -150,7 +150,7 @@ namespace RemoteTech.UI
                 {
                     GUILayout.FlexibleSpace();
                     GuiUtil.Button(new GUIContent(">>", "Toggles the queue and delay functionality."),
-                        mOnClickQueue, GUILayout.Width(width3));
+                        _onClickQueue, GUILayout.Width(width3));
                 }
                 GUILayout.EndHorizontal();
             }
@@ -161,14 +161,14 @@ namespace RemoteTech.UI
         {
 
             if (Event.current.Equals(Event.KeyboardEvent("return")) && Speed != 0) {
-                if (GUI.GetNameOfFocusedControl() == "RC1" && mSteering != 0 && Turn != 0)
+                if (GUI.GetNameOfFocusedControl() == "RC1" && _steering != 0 && Turn != 0)
                     EnqueueTurn();
                 else if (GUI.GetNameOfFocusedControl() == "RC2" && Dist != 0)
                     EnqueueDist();
                 else if (GUI.GetNameOfFocusedControl() == "RC3") {
-                    if (!distDefault && mSteering != 0 && Turn != 0)
+                    if (!_distDefault && _steering != 0 && Turn != 0)
                         EnqueueTurn();
-                    else if (distDefault && Dist != 0)
+                    else if (_distDefault && Dist != 0)
                         EnqueueDist();
                 }
             }
@@ -178,9 +178,9 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Wheel: ", "how much to turn"));
                 GUILayout.FlexibleSpace();
-                GUILayout.Label(new GUIContent(Math.Abs(mSteering).ToString("P"), "How much to turn"));
-                if (mSteering != 0) {
-                    if (mSteering < 0)
+                GUILayout.Label(new GUIContent(Math.Abs(_steering).ToString("P"), "How much to turn"));
+                if (_steering != 0) {
+                    if (_steering < 0)
                         GUILayout.Label(new GUIContent("right", "how much to turn"), GUILayout.Width(40));
                     else
                         GUILayout.Label(new GUIContent("left", "how much to turn"), GUILayout.Width(40));
@@ -189,13 +189,13 @@ namespace RemoteTech.UI
             }
             GUILayout.EndHorizontal();
 
-            GuiUtil.HorizontalSlider(ref mSteering, 1, -1);
+            GuiUtil.HorizontalSlider(ref _steering, 1, -1);
 
             GUILayout.BeginHorizontal();
             {
                 GUILayout.Label(new GUIContent("Turn", "How many degrees to turn"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC1");
-                GuiUtil.TextField(ref mTurn, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _turnString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(°)", "How many degrees to turn"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
@@ -204,7 +204,7 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Dist.", "Distance to drive"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC2");
-                GuiUtil.TextField(ref mDist, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _distanceString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(m)", "Distance to drive"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
@@ -213,23 +213,23 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Speed", "Speed to keep, negative for reverse"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC3");
-                GuiUtil.TextField(ref mSpeed, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _speedString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(m/s)", "Speed to keep, negative for reverse"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
 
-            mTurn = FormatUtil.ConstrictNum(mTurn, 90);
-            mDist = FormatUtil.ConstrictNum(mDist, false);
-            mSpeed = FormatUtil.ConstrictNum(mSpeed);
+            _turnString = FormatUtil.ConstrictNum(_turnString, 90);
+            _distanceString = FormatUtil.ConstrictNum(_distanceString, false);
+            _speedString = FormatUtil.ConstrictNum(_speedString);
 
 
-            if (prevTurn != Turn)
-                distDefault = false;
-            else if (prevDist != Dist)
-                distDefault = true;
+            if (_prevTurn != Turn)
+                _distDefault = false;
+            else if (_prevDist != Dist)
+                _distDefault = true;
 
-            prevTurn = Turn;
-            prevDist = Dist;
+            _prevTurn = Turn;
+            _prevDist = Dist;
 
         }
 
@@ -237,24 +237,24 @@ namespace RemoteTech.UI
         {
 
             if (Event.current.Equals(Event.KeyboardEvent("return")) && GUI.GetNameOfFocusedControl().StartsWith("RC"))
-                mFlightComputer.Enqueue(DriveCommand.DistanceHeading(Dist, Heading, mSteerClamp, Speed));
+                _flightComputer.Enqueue(DriveCommand.DistanceHeading(Dist, Heading, _steerClamp, Speed));
 
             GUILayout.BeginHorizontal();
             {
                 GUILayout.Label(new GUIContent("Wheel: ", "How sharp to turn at max"));
                 GUILayout.FlexibleSpace();
-                GUILayout.Label(new GUIContent(mSteerClamp.ToString("P"), "How sharp to turn at max"));
+                GUILayout.Label(new GUIContent(_steerClamp.ToString("P"), "How sharp to turn at max"));
                 GUILayout.Label(new GUIContent("max", "How sharp to turn at max"), GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
 
-            GuiUtil.HorizontalSlider(ref mSteerClamp, 0, 1);
+            GuiUtil.HorizontalSlider(ref _steerClamp, 0, 1);
 
             GUILayout.BeginHorizontal();
             {
                 GUILayout.Label(new GUIContent("Hdg.", "Heading to keep"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC1");
-                GuiUtil.TextField(ref Mheading, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _headingString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label("(°)", GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
@@ -263,7 +263,7 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Dist.", "Distance to drive"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC2");
-                GuiUtil.TextField(ref mDist, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _distanceString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label("(m)", GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
@@ -272,29 +272,29 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Speed", "Speed to keep"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC3");
-                GuiUtil.TextField(ref mSpeed, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _speedString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(m/s)", "Speed to keep"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
 
-            Mheading = FormatUtil.ConstrictNum(Mheading, 360);
-            mDist = FormatUtil.ConstrictNum(mDist, false);
-            mSpeed = FormatUtil.ConstrictNum(mSpeed, false);
+            _headingString = FormatUtil.ConstrictNum(_headingString, 360);
+            _distanceString = FormatUtil.ConstrictNum(_distanceString, false);
+            _speedString = FormatUtil.ConstrictNum(_speedString, false);
         }
 
         private void Target()
         {
             if (Event.current.Equals(Event.KeyboardEvent("return")) && GUI.GetNameOfFocusedControl().StartsWith("RC"))
-                mFlightComputer.Enqueue(DriveCommand.Coord(mSteerClamp, Latitude, Longitude, Speed));
-            else if (GameSettings.MODIFIER_KEY.GetKey() && ((Input.GetMouseButton(0) || Input.GetMouseButton(1)) != MouseClick)) {
-                MouseClick = Input.GetMouseButton(0) || Input.GetMouseButton(1);
+                _flightComputer.Enqueue(DriveCommand.Coord(_steerClamp, Latitude, Longitude, Speed));
+            else if (GameSettings.MODIFIER_KEY.GetKey() && ((Input.GetMouseButton(0) || Input.GetMouseButton(1)) != _mouseClick)) {
+                _mouseClick = Input.GetMouseButton(0) || Input.GetMouseButton(1);
                 Vector2 latlon;
-                if (MouseClick && CelestialBodyExtension.CBhit(mFlightComputer.Vessel.mainBody, out latlon)) {
+                if (_mouseClick && CelestialBodyExtension.CBhit(_flightComputer.Vessel.mainBody, out latlon)) {
                     Latitude = latlon.x;
                     Longitude = latlon.y;
 
                     if (Input.GetMouseButton(1))
-                        mFlightComputer.Enqueue(DriveCommand.Coord(mSteerClamp, Latitude, Longitude, Speed));
+                        _flightComputer.Enqueue(DriveCommand.Coord(_steerClamp, Latitude, Longitude, Speed));
                 }
             }
 
@@ -302,18 +302,18 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Wheel: ", "How sharp to turn at max"));
                 GUILayout.FlexibleSpace();
-                GUILayout.Label(new GUIContent(mSteerClamp.ToString("P"), "How sharp to turn at max"));
+                GUILayout.Label(new GUIContent(_steerClamp.ToString("P"), "How sharp to turn at max"));
                 GUILayout.Label(new GUIContent("max", "How sharp to turn at max"), GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
 
-            GuiUtil.HorizontalSlider(ref mSteerClamp, 0, 1);
+            GuiUtil.HorizontalSlider(ref _steerClamp, 0, 1);
 
             GUILayout.BeginHorizontal();
             {
                 GUILayout.Label(new GUIContent("LAT.", "Latitude to drive to"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC1");
-                GuiUtil.TextField(ref mLatitude, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _latitudeString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(°)", "Hold " + GameSettings.MODIFIER_KEY.name + " and click on ground to input coordinates"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
@@ -322,7 +322,7 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("LON.", "Longitude to drive to"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC2");
-                GuiUtil.TextField(ref mLongditude, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _longitudeString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(°)", "Hold " + GameSettings.MODIFIER_KEY.name + " and click on ground to input coordinates"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
@@ -331,15 +331,15 @@ namespace RemoteTech.UI
             {
                 GUILayout.Label(new GUIContent("Speed", "Speed to keep"), GUILayout.Width(50));
                 GUI.SetNextControlName("RC3");
-                GuiUtil.TextField(ref mSpeed, GUILayout.Width(50), GUILayout.ExpandWidth(false));
+                GuiUtil.TextField(ref _speedString, GUILayout.Width(50), GUILayout.ExpandWidth(false));
                 GUILayout.Label(new GUIContent("(m/s)", "Speed to keep"), GUI.skin.textField, GUILayout.Width(40));
             }
             GUILayout.EndHorizontal();
 
 
-            mLatitude = FormatUtil.ConstrictNum(mLatitude);
-            mLongditude = FormatUtil.ConstrictNum(mLongditude);
-            mSpeed = FormatUtil.ConstrictNum(mSpeed, false);
+            _latitudeString = FormatUtil.ConstrictNum(_latitudeString);
+            _longitudeString = FormatUtil.ConstrictNum(_longitudeString);
+            _speedString = FormatUtil.ConstrictNum(_speedString, false);
         }
 
     }
