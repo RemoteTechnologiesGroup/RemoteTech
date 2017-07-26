6e61d130e0d041865f1596ed4ec485b80a6fb263
Yamori Yuki <yamori.yuki@flamingfly.org>
2017-07-26 15:48:06 +0200

    Fix the issue of science transmission ignoring the timewrap (#731)
    Change the transmission timing to be based on fixed updates and timewarp instead of
    coroutine wait time

--- src/RemoteTech/Modules/ModuleRTDataTransmitter.cs
+++ src/RemoteTech/Modules/ModuleRTDataTransmitter.cs
@@ -23,6 +23,7 @@ namespace RemoteTech.Modules
 
         private bool isBusy;
         private readonly List<ScienceData> scienceDataQueue = new List<ScienceData>();
+        private double timeElapsed = 0;
 
         // Compatible with ModuleDataTransmitter
         public override void OnLoad(ConfigNode node)
@@ -64,6 +65,14 @@ namespace RemoteTech.Modules
         double IScienceDataTransmitter.DataResourceCost { get { return PacketResourceCost / PacketSize; } }
         bool IScienceDataTransmitter.IsBusy() { return isBusy; }
 
+        public void FixedUpdate()
+        {
+            if (isBusy)
+            {
+                timeElapsed += TimeWarp.fixedDeltaTime;
+            }
+        }
+
         private IEnumerator Transmit(Callback callback = null)
         {
             RTLog.Notify("ModuleRTDataTransmitter::Transmit");
@@ -91,89 +100,94 @@ namespace RemoteTech.Modules
                 {
                     // pre-calculate the time interval - fix for x64 systems
                     // workaround for issue #136
-                    float time1 = Time.time;
-                    yield return new WaitForSeconds(PacketInterval);
+                    //float time1 = Time.time;
+                    //yield return new WaitForSeconds(PacketInterval);
 
                     // get the delta time
-                    float x64PacketInterval = (Time.time - time1);
-                    RTLog.Notify("Changing RnDCommsStream timeout from {0} to {1}", PacketInterval, x64PacketInterval);
+                    //float x64PacketInterval = (Time.time - time1);
+                    //RTLog.Notify("Changing RnDCommsStream timeout from {0} to {1}", PacketInterval, x64PacketInterval);
 
                     //TODO (porting to 1.2): check if scienceData.baseTransmitValue alone or with scienceData.transmitBonus
-                    commStream = new RnDCommsStream(subject, scienceData.dataAmount, x64PacketInterval,
+                    //commStream = new RnDCommsStream(subject, scienceData.dataAmount, x64PacketInterval,
+                    commStream = new RnDCommsStream(subject, scienceData.dataAmount, PacketInterval,
                                             scienceData.baseTransmitValue, false, ResearchAndDevelopment.Instance);
                 }
                 //StartCoroutine(SetFXModules_Coroutine(modules_progress, 0.0f));
                 float power = 0;
+                timeElapsed = 0;
                 while (packets > 0)
                 {
-                    power += part.RequestResource("ElectricCharge", PacketResourceCost - power);
-                    if (power >= PacketResourceCost * 0.95)
+                    if (timeElapsed >= PacketInterval)
                     {
-                        GUIStatus = "Uploading Data...";
-
-                        // remove some power due to transmission
-                        power -= PacketResourceCost;
-
-                        // transmitted size
-                        float frame = Math.Min(PacketSize, dataAmount);
-
-                        // subtract current packet size from data left to transmit
-                        // and clamp it to 1 digit precision to avoid large float precision error (#667)
-                        dataAmount -= frame;
-                        dataAmount = (float)Math.Round(dataAmount, 1);
-
-                        packets--;
-
-                        float progress = (scienceData.dataAmount - dataAmount) / scienceData.dataAmount;
-                        msgStatus.message = String.Format("[{0}]: Uploading Data... {1:P0}", part.partInfo.title, progress);
-                        ScreenMessages.PostScreenMessage(msgStatus);
-
-                        RTLog.Notify("[Transmitter]: Uploading Data... ({0}) - {1} Mits/sec. Packets to go: {2} - Other experiments waiting to transfer: {3}",
-                            scienceData.title, (PacketSize / PacketInterval).ToString("0.00"), packets, scienceDataQueue.Count);
-
-                        // if we've a defined callback parameter so skip to stream each packet
-                        if (commStream != null && callback == null)
+                        timeElapsed -= PacketInterval;
+                        power += part.RequestResource("ElectricCharge", PacketResourceCost - power);
+                        if (power >= PacketResourceCost * 0.95)
                         {
-                            RTLog.Notify(
-                                "[Transmitter]: PacketSize: {0}; Transmitted size (frame): {1}; Data left to transmit (dataAmount): {2}; Packets left (packets): {3}",
-                                PacketSize, frame, dataAmount, packets);
+                            GUIStatus = "Uploading Data...";
 
-                            // use try / catch to prevent NRE spamming in KSP code when RT is used with other mods.
-                            try
+                            // remove some power due to transmission
+                            power -= PacketResourceCost;
+
+                            // transmitted size
+                            float frame = Math.Min(PacketSize, dataAmount);
+
+                            // subtract current packet size from data left to transmit
+                            // and clamp it to 1 digit precision to avoid large float precision error (#667)
+                            dataAmount -= frame;
+                            dataAmount = (float)Math.Round(dataAmount, 1);
+
+                            packets--;
+
+                            float progress = (scienceData.dataAmount - dataAmount) / scienceData.dataAmount;
+                            msgStatus.message = String.Format("[{0}]: Uploading Data... {1:P0}", part.partInfo.title, progress);
+                            ScreenMessages.PostScreenMessage(msgStatus);
+
+                            RTLog.Notify("[Transmitter]: Uploading Data... ({0}) - {1} Mits/sec. Packets to go: {2} - Other experiments waiting to transfer: {3}",
+                                scienceData.title, (PacketSize / PacketInterval).ToString("0.00"), packets, scienceDataQueue.Count);
+
+                            // if we've a defined callback parameter so skip to stream each packet
+                            if (commStream != null && callback == null)
                             {
-                                // check if it's the last packet to send
-                                if (packets == 0)
+                                RTLog.Notify(
+                                    "[Transmitter]: PacketSize: {0}; Transmitted size (frame): {1}; Data left to transmit (dataAmount): {2}; Packets left (packets): {3}",
+                                    PacketSize, frame, dataAmount, packets);
+
+                                // use try / catch to prevent NRE spamming in KSP code when RT is used with other mods.
+                                try
                                 {
-                                    // issue #667, issue #714 ; floating point error in RnDCommsStream.StreamData method when adding to dataIn private field
-                                    // e.g scienceData.dataAmount is 10 but in the end RnDCommsStream.dataIn will be 9.999999, so the science never
-                                    //     gets registered to the ResearchAndDevelopment center.
-                                    // Let's just go ahead and send the full amount so there's no question if we sent it all
-                                    // KSP will clamp the final size to PacketSize anyway.
-                                    frame = scienceData.dataAmount;
-                                }
+                                    // check if it's the last packet to send
+                                    if (packets == 0)
+                                    {
+                                        // issue #667, issue #714 ; floating point error in RnDCommsStream.StreamData method when adding to dataIn private field
+                                        // e.g scienceData.dataAmount is 10 but in the end RnDCommsStream.dataIn will be 9.999999, so the science never
+                                        //     gets registered to the ResearchAndDevelopment center.
+                                        // Let's just go ahead and send the full amount so there's no question if we sent it all
+                                        // KSP will clamp the final size to PacketSize anyway.
+                                        frame = scienceData.dataAmount;
+                                    }
 
-                                commStream.StreamData(frame, vessel.protoVessel);
+                                    commStream.StreamData(frame, vessel.protoVessel);
+                                }
+                                catch (NullReferenceException nre)
+                                {
+                                    RTLog.Notify("[Transmitter] A problem occurred during science transmission: {0}", RTLogLevel.LVL2, nre);
+                                }
                             }
-                            catch (NullReferenceException nre)
+                            else
                             {
-                                RTLog.Notify("[Transmitter] A problem occurred during science transmission: {0}", RTLogLevel.LVL2, nre);
+                                RTLog.Notify("[Transmitter]: [DEBUG] commstream is null and no callback");
                             }
                         }
                         else
                         {
-                            RTLog.Notify("[Transmitter]: [DEBUG] commstream is null and no callback");
+                            // not enough power
+                            msg.message = String.Format("<b><color=orange>[{0}]: Warning! Not Enough {1}!</color></b>", part.partInfo.title, RequiredResource);
+                            ScreenMessages.PostScreenMessage(msg);
+
+                            GUIStatus = String.Format("{0}/{1} {2}", power, PacketResourceCost, RequiredResource);
                         }
                     }
-                    else
-                    {
-                        // not enough power
-                        msg.message = String.Format("<b><color=orange>[{0}]: Warning! Not Enough {1}!</color></b>", part.partInfo.title, RequiredResource);
-                        ScreenMessages.PostScreenMessage(msg);
-
-                        GUIStatus = String.Format("{0}/{1} {2}", power, PacketResourceCost, RequiredResource);
-                    }
-
-                    yield return new WaitForSeconds(PacketInterval);
+                    yield return new WaitForFixedUpdate();
                 }
 
                 // effectively inform the game that science has been transmitted
