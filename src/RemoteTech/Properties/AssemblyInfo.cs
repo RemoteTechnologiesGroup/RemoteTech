f6c06c1c172b9b6616d40f6770bbe5d0abd51298
KSP-TaxiService <chinglihlim@gmail.com>
2018-12-24 23:13:32 +0800

    Bump release version to 1.9.1

--- src/RemoteTech/Properties/AssemblyInfo.cs
+++ src/RemoteTech/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RemoteTechnologiesGroup")]
 [assembly: AssemblyProduct("RemoteTech")]
-[assembly: AssemblyCopyright("Copyright © 2013-2018")]
+[assembly: AssemblyCopyright("Copyright © 2013-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // DLLs any time it changes.  Breaking on a minor revision is probably acceptable - it's
 // unlikely that there wouldn't be other breaking changes on a minor version change.
 [assembly: AssemblyVersion("1.9")]
-[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.1")]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
